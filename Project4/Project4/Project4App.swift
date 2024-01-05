f75e225a6a236c45dd3b1a618b06e2afde6787ef
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2024-01-05 15:48:45 +0300

    Add files via upload

---
+++ Project4/Project4/Project4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project4App.swift
+//  Project4
+//
+//  Created by gayeugur on 4.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
