6b26877dc9bbe98ed44340e6242dd9d0435998cd
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2024-01-06 22:17:02 +0300

    Add files via upload

---
+++ Project7/Project7/AddView.swift
@@ -0,0 +1,57 @@
+//
+//  AddView.swift
+//  Project7
+//
+//  Created by gayeugur on 6.01.2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var currencyCode = "USD"
+    @State private var amount = 0.0
+
+    var expenses: Expenses
+
+    let types = ["Business", "Personal"]
+    let moneyCode = ["USD", "EUR", "TL"]
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                Picker("Money Code", selection: $currencyCode) {
+                    ForEach(moneyCode, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount, currencyCode: currencyCode)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
---
+++ Project7/Project7/Project7App.swift
@@ -0,0 +1,17 @@
+//
+//  Project7App.swift
+//  Project7
+//
+//  Created by gayeugur on 5.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Project7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
