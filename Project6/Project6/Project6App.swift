7d52c93c1bc5afe17bb824e235bd3f182be3db76
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2024-01-06 22:16:30 +0300

    Add files via upload

---
+++ Project6/Project6/Project6App.swift
@@ -0,0 +1,17 @@
+//
+//  Project6App.swift
+//  Project6
+//
+//  Created by gayeugur on 5.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Project6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
