6c510a1803b7aed7721923069a0263db8df2fd76
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2024-01-07 19:15:21 +0300

    Add files via upload

---
+++ App/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project8App.swift
+//  Project8
+//
+//  Created by gayeugur on 6.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Model/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Project8
+//
+//  Created by gayeugur on 7.01.2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
