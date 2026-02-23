46a6d585060b8cfa9ec0309f6a44f0af7b443e47
Gaye Ugur <gayeugur00@gmail.com>
2026-02-23 20:56:43 +0300

    refactor

---
+++ Project13/Project13/Project13/Project13App.swift
@@ -0,0 +1,17 @@
+//
+//  Project13App.swift
+//  Project13
+//
+//  Created by gayeugur on 23.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
