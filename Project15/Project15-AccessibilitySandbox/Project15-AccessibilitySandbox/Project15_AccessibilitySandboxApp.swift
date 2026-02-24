c30a2efb68c2beefe75443aa592feb077f504414
Gaye Ugur <gayeugur00@gmail.com>
2026-02-24 13:08:49 +0300

    refactor

---
+++ Project15/Project15-AccessibilitySandbox/Project15-AccessibilitySandbox/Project15_AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project15_AccessibilitySandboxApp.swift
+//  Project15-AccessibilitySandbox
+//
+//  Created by gayeugur on 24.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project15_AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
