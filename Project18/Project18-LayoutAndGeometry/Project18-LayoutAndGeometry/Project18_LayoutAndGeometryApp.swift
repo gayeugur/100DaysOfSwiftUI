4c877a490e16880a2ffb3a514cbc0bcc43ba463b
Gaye Ugur <gayeugur00@gmail.com>
2026-02-25 15:56:32 +0300

    refactor

---
+++ Project18/Project18-LayoutAndGeometry/Project18-LayoutAndGeometry/Project18_LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project18_LayoutAndGeometryApp.swift
+//  Project18-LayoutAndGeometry
+//
+//  Created by gayeugur on 25.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project18_LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
