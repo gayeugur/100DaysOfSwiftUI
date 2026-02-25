67582d0f57136afa4fb2aa6ffc88f2bbdd82c865
Gaye Ugur <gayeugur00@gmail.com>
2026-02-25 20:25:24 +0300

    refactor

---
+++ Project19/Project19-SnowSeeker/Project19-SnowSeeker/Project19_SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project19_SnowSeekerApp.swift
+//  Project19-SnowSeeker
+//
+//  Created by gayeugur on 25.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Project19_SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
